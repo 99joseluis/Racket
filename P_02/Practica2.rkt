89714541acfd266b8d7b0669eca0cf03803697a8
99joseluis <jose_luis@ciencias.unam.mx>
2020-10-21 11:35:51 -0500

    Combinando este archivo con el archivo ejercicio1al7.rkt

--- P_02/Practica2.rkt
+++ P_02/Practica2.rkt
@@ -29,7 +29,11 @@
 ;; Ejercicio 5
 ;; Función que recibe una lista l con elementos. Devuelve una lista sin repeticiones con los elementos de l.
 ;; eliminaRep: (listof a) -> (listof a)
-(define (eliminaRepetidos lista)...)
+(define (eliminaRepetidos lista)
+  (foldr (lambda (elemento lista)
+           (if (not (pertenece? elemento lista))
+               (cons elemento lista) lista)) '() lista))
+
 
 ;; Estructura que nos permite modelar puntos en el plano.
 ;; Sirve para modelar figuras geométricas.
@@ -39,13 +43,19 @@
 ;; Funcion que nos permite calcular el punto equidistante entre dos puntos.
 ;; Si alguno de los dos parámetros recibidos no es un punto, lanza un error
 ;; punto-medio: Punto Punto -> Punto
-(define (punto-medio p q) ...)
+(define (punto-medio p q)
+  (if (not(Punto? p))(error "P no es un punto" )
+   (if (not(Punto? q))(error  "Q no es un punto" ) 
+  (Punto(quotient(+ (Punto-x p) (Punto-x q)) 2) (quotient(+ (Punto-y p)(Punto-y q)) 2)))))
 
 ;; Ejercicio 7
 ;; Funcion que nos permite calcular la distancia entre dos puntos.
 ;; Si alguno de los dos parámetros recibidos no es un punto, lanza un error
 ;; distancia: Punto Punto -> number
-(define (distancia p q)...)
+(define (distancia p q)
+  (if (not(Punto? p))(error "P no es un punto" )
+   (if (not(Punto? q))(error  "Q no es un punto" ) 
+  (sqrt(+ (expt (- (Punto-x p) (Punto-x q)) 2) (expt (- (Punto-y p) (Punto-y q)) 2) )))))
 
 ;; Ejercicio 8
 ;; Definición del tipo abstracto de datos Figura
@@ -69,15 +79,15 @@
 ;; Función que nos da el elemento más repetido en una lista. 
 ;; Si hay dos o más elementos repetidos el mismo número de veces, devuelve el primero en aparecer de izquierda a derecha.
 ;; masRepetido (listof a) -> number
-;(define (masRepetido lista)
+(define (masRepetido lista) ... )
 
 
 (define a (Punto 2 2))
 (define b (Punto 2 8))
-(define c (Circulo (Punto 0 0) 1))
+;;(define c (Circulo (Punto 0 0) 1))
 
 
-(test (perimetro c) 6.283185307179586)
+;;(test (perimetro c) 6.283185307179586)
 (test (distancia a b) 6)
 (test (punto-medio a b) (Punto 2 5))
 
