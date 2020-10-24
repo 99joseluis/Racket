821a18ef6d05dc9060fc1f947ba521b425950c3e
99joseluis <jose_luis@ciencias.unam.mx>
2020-10-23 22:25:34 -0500

    agregando mejoras a la practica y el test

--- P_02/Practica2.rkt
+++ P_02/Practica2.rkt
@@ -30,9 +30,10 @@
 ;; Función que recibe una lista l con elementos. Devuelve una lista sin repeticiones con los elementos de l.
 ;; eliminaRep: (listof a) -> (listof a)
 (define (eliminaRepetidos lista)
-  (foldr (lambda (elemento lista)
-           (if (not (pertenece? elemento lista))
-               (cons elemento lista) lista)) '() lista))
+  (match lista
+    ['() empty]
+    [(cons x xs) (cons x (eliminaRepetidos (filter (lambda (y) (not (equal? x y))) xs)))]
+    ))
 
 
 ;; Estructura que nos permite modelar puntos en el plano.
@@ -45,8 +46,8 @@
 ;; punto-medio: Punto Punto -> Punto
 (define (punto-medio p q)
   (if (not(Punto? p))(error "P no es un punto" )
-   (if (not(Punto? q))(error  "Q no es un punto" ) 
-  (Punto(quotient(+ (Punto-x p) (Punto-x q)) 2) (quotient(+ (Punto-y p)(Punto-y q)) 2)))))
+   (if (not(Punto? q))(error  "Q no es un punto" )
+  (Punto(/ (+ (Punto-x p) (Punto-x q)) 2) (/ (+ (Punto-y p)(Punto-y q)) 2)))))
 
 ;; Ejercicio 7
 ;; Funcion que nos permite calcular la distancia entre dos puntos.
@@ -60,31 +61,57 @@
 ;; Ejercicio 8
 ;; Definición del tipo abstracto de datos Figura
 (define-type Figura
-  [Circulo ... ]
-  [Triangulo ... ]
-  [Rectangulo ... ]
-  [Cuadrado ...])
+  ;Crear constructores de cada tipo
+  [Circulo (p Punto?) (radio positive?)]
+  [Triangulo (p1 Punto?) (p2 Punto?) (p3 Punto?)]
+  [Rectangulo (p Punto?) (base positive?) (alt positive?) ] 
+  [Cuadrado (p Punto?) (lado positive?)])
 
 ;; Ejercicio 9
 ;; Función que recibe una figura y calcula su perímetro.
 ;; perimetro: Figura -> number
-(define (perimetro fig)...)
+(define (perimetro fig)
+ (type-case Figura fig
+  [Circulo (p radio) (* (* 2 pi) radio)] 
+  [Triangulo (p1 p2 p3) (+ (distancia p1 p2) (distancia p1 p3) (distancia p2 p3))]   
+  [Rectangulo (p base alt) (+ (expt 2 base) (expt 2 alt)) ]
+  [Cuadrado (p lado) (* 4 lado)] 
+  ))
 
 ;; Ejercicio 10
 ;; Función que recibe una figura y calcula su área.
 ;; area: Figura -> number
-(define (area fig) ...)
+(define (area fig)
+  (type-case Figura fig
+   [Circulo (p radio) (* pi (* radio radio) )]
+   [Triangulo (p1 p2 p3)(let ([s (/ (+ (distancia p1 p2) (distancia p1 p3)(distancia p2 p3)) 2)] [t 4])
+                                                       (sqrt (* s (- s (distancia p1 p2)) (- s (distancia p1 p3)) (- s (distancia p2 p3)))))]
+   [Rectangulo (p base alt) (/ (* base alt) 2)]
+   [Cuadrado (p lado) (* lado lado)]
+    ))  
 
 ;; Punto extra
 ;; Función que nos da el elemento más repetido en una lista. 
 ;; Si hay dos o más elementos repetidos el mismo número de veces, devuelve el primero en aparecer de izquierda a derecha.
 ;; masRepetido (listof a) -> number
-(define (masRepetido lista) ... )
+(define (masRepetido lista)
+  (lambda (x) (
+
+;;Funcion que te da la suma de las repeticiones de l en una lista
+;; repetido symbol (listof ) -> int
+(define (repetido l lista)
+  (match lista
+    ['() 0]
+    [(cons x xs) (if (equal? x l)
+                     (+ 1 (repetido l (cdr lista)))
+                     (repetido l (cdr lista)))]))
+
 
 
 (define a (Punto 2 2))
 (define b (Punto 2 8))
-;;(define c (Circulo (Punto 0 0) 1))
+(define c (Circulo (Punto 0 0) 1))
+(define d (Circulo (Punto 0 0) 2))
 
 
 ;;(test (perimetro c) 6.283185307179586)
